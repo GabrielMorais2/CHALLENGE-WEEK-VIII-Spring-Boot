180a0d0272796fbf69d69919eb2ce65439ea9a72
Gabriel Moraes <moraisgabriel274@gmail.com>
2023-08-08 23:36:26 -0300

    Unused class withdrawal.

--- src/main/java/gabriel/moraes/school/exception/ClassRoomNotFound.java
+++
@@ -1,7 +0,0 @@
-package gabriel.moraes.school.exception;
-
-public class ClassRoomNotFound extends RuntimeException{
-    public ClassRoomNotFound(String message){
-        super(message);
-    }
-}
